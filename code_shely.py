fdc65b5acaac4af30eb3df5c9550f76460e06517
shely2610 <94021635+shely2610@users.noreply.github.com>
2021-11-20 15:39:52 -0800

    Update code_shely.py

--- code_shely.py
+++ code_shely.py
@@ -4,8 +4,8 @@ import time
 
 r = r()
 
-def accx():
-    
+#def accx():
+
 while True:
     accx = cp.acceleration.x
     print(accx)
@@ -13,7 +13,9 @@ while True:
     if accx <= -5.0 and accx >= -9.8:
         r.send("left")
         print("left")
+        time.sleep(1)
 
     if accx <= 9.8 and accx >= 5.0:
         r.send("right")
         print("right")
+        time.sleep(1)
